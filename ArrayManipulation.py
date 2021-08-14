598bf24b80e4e03dd00b17fbf4a456811b63618c
Bharath Prathap Nair <51161902+bharathprathap@users.noreply.github.com>
2021-08-14 17:35:59 +0530

    Create ArrayManipulation.py

---
+++ ArrayManipulation.py
@@ -0,0 +1,23 @@
+#BHARATH PRATHAP NAIR
+#Problem:Array Manipulation
+#https://www.hackerrank.com/challenges/crush/problem
+
+def arrayManipulation(n, queries):
+    arr=[0]*(n+1)
+    
+    for q in queries :
+        a=q[0]
+        b=q[1]
+        k=q[2]
+        arr[a-1]+=k
+        if(b<len(arr)):
+            arr[b]-=k
+            
+    sum=0
+    max=0
+    for i in arr:
+        sum+=i
+        if sum>max:
+            max=sum
+
+    return max
